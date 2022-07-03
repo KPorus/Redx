64a8a2aa01331145540c66585d8bdb60037c3d34
KPorus <designerfardin2021@gmail.com>
2022-07-03 09:18:48 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(function (){
+    $('.sliderp').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+      });
+});
